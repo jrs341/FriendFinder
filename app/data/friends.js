a534093a7fa36661f53032ee7b7784af3e6bbcfa
Jason Sinn <jrs341@me.com>
2016-12-27 13:34:38 -0600

    updating mvc

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,97 +1,108 @@
 // ===============================================================================
 // DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
+// Below data will hold all of the possible friends.
 // ===============================================================================
 
 
-var profileArray = [
-	{
-		name: 'Ahmed',
-		photo: 'ahmed@example.com',
-		scores: [5,4,3]
+var friends = [
+  {
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Jacob Deming",
+    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Jeremiah Scanlon",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Louis T. Delia",
+    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Lou Ritter",
+    photo: "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Jordan Biason",
+    photo: "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
 
-	},{
-		name:'Jason',
-		photo: 'at some link',
-		scores: [3,4,5]
-	},{
-		name: 'Mari',
-		photo: 'at some link',
-		scores: [7,8,9]
-	},{
-		name: 'Bob',
-		photo: 'at some link',
-		scores: [5,6,7]
-	},{
-		name: 'Heidi',
-		photo: 'at some link',
-		scores: [3,4,5]
-	}
 ];
 
-module.exports = profileArray;
-
-var totalDifference = [];
-
-var min = [];
-
-var positionArray = [];
-
-var i = 1;
-
-function add (a, b) {
-	return a + b;
-}
-
-// This function grabs each element object in the profile array
-
-function getArray() {
-
-	for (i ; i < profileArray.length; i++) {
-		arrayDifference();
-	}
-}
-
-// This function determines the difference between the users profile and all of the saved profiles
-
-function arrayDifference() {
-	var difference = [];
-	for (var x = 0; x < profileArray[0].scores.length; x++) {
-		var y = Math.abs(profileArray[0].scores[x] - profileArray[i].scores[x]);
-		difference.push(y);
-	}
-	console.log(difference);
-	var total = difference.reduce(add, 0);
-	// console.log(total);
-	totalDifference.push(total);
-	// console.log(totalDifference.join(', '));
-}
-
-// This function finds the minimum number or numbers in the position array and returns their position which will correspond to the profile array position i-1
-
-// Need to fix this function to find equal min numbers
-
-function findMatch() {
-
-	for (var i = 0; i < totalDifference.length; i++) {
-		min.push(Math.min.apply(Math, totalDifference));
-		// var position = totalDifference.indexOf(min);
-		// console.log(position);
-
-	}
-	
-	// var position = totalDifference.indexOf(min);
-	// positionArray.push(position);
-	// console.log(position);
-	// console.log(positionArray);
-}
-
-getArray();
-findMatch();
-console.log(min);
-
-// console.log(min);
-// console.log(positionArray);
-
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
